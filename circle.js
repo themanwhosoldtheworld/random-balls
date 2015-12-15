94511e7902252cc7729b55b276c0527ea4d69a75
themanwhosoldtheworld <kalyanayyagari@gmail.com>
2015-12-15 23:42:41 +0530

    refining corner cases

--- circle.js
+++ circle.js
@@ -21,13 +21,30 @@ var yBorder = ((this.y+this.rad) >= (height + gap)) || ((this.y-this.rad) <= gap
 if(xBorder && yBorder){
 	this.deltaX = (-1)*this.deltaX;
 	this.deltaY = (-1)*this.deltaY;
-}else if( xBorder){
+}else if(xBorder){
 	this.deltaX = (-1)*this.deltaX;
 }else if( yBorder){
 	this.deltaY = (-1)*this.deltaY;
 }
-this.x= this.x + this.deltaX;
-this.y= this.y + this.deltaY;
+var newX = (this.x + this.deltaX);
+if((this.x + this.deltaX) <=0){
+	newX = this.rad + gap;
+}else if((this.x + this.deltaX)>=width){
+	newX = width - this.rad - gap;
+}
+//var newX= (this.x + this.deltaX)>0?(this.x + this.deltaX):(this.rad + gap);
+//newX= (this.x + this.deltaX)<width?(this.x + this.deltaX):(width - this.rad - gap);
+this.x = newX;
+
+var newY = (this.y + this.deltaY);
+if((this.y + this.deltaY) <=0){
+	newY = this.rad + gap;
+}else if((this.y + this.deltaY)>=height){
+	newY = height - this.rad - gap;
+}
+//var newY= (this.y + this.deltaY)>0?(this.y + this.deltaY):(this.rad + gap);
+//newY= (this.y + this.deltaY)<height?(this.y + this.deltaY):(height - this.rad - gap);
+this.y = newY;
 ctx.restore();
 }
 }
--- start.js
+++ start.js
@@ -1,13 +1,13 @@
 var delta = 1;
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-var width = 500;
-var height = 500;
+var width = 1200;
+var height = 600;
 var gap = .5;
 var circles = [];
-for(var i=0;i<10;i++){
-	var cx = Math.floor((Math.random() * 90) + 10);
-	var cy = Math.floor((Math.random() * 90) + 10);
+for(var i=0;i<20;i++){
+	var cx = Math.floor((Math.random() * (width-10)),10);
+	var cy = Math.floor((Math.random() * (height-10)),10);
 	var dx = Math.floor((Math.random() * 5));
 	var dy = Math.floor((Math.random() * 5));
 	circles.push(new Circle(i,cx,cy,10,dx,dy));
@@ -16,8 +16,8 @@ for(var i=0;i<10;i++){
 //var c2 = new Circle(100,100,10,-delta,-delta);
 function draw(){
 ctx.fillRect(0,0,width,height);
-for(var i in circles){
-	CollisionStrategy(circles);
+CollisionStrategy(circles);
+for(var i in circles){	
 	circles[i].draw(ctx);
 }
 }
